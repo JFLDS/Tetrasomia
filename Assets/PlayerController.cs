71aae512af343dd6206916939781947374d2b98e
ZEITTON <84009797+ZEITTON@users.noreply.github.com>
2021-05-11 12:43:29 +0200

    Add_Player
    on a ajouté le Player et un script afin de calculer la vélocité pour pouvoir faire le déplacement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor motor;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Calcul de la vélocité                     //renvoie 0 si on ne bouge pas
+        float xM = Input.GetAxisRaw("Horizontal");  //Q(touche négative)=(-1) & D(touche positive)=(1)
+        float zM = Input.GetAxisRaw("Vertical");    //S(touche négative)=(-1) & Z(touche positive)=(1)
+
+        Vector3 moveHorizontal = transform.right * xM;
+        Vector3 moveVertical = transform.forward * zM;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+    }
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
