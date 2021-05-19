1997a13a299bce7afef94ed410055a2a83c009f9
Swaaze <84011746+Swaaze@users.noreply.github.com>
2021-05-19 15:56:53 +0200

    Debut Gestion tir

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon weapon;
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camera == null) Debug.LogError("Pas de caméra."); this.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shoot();
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, weapon.range, mask))
+        {
+            Debug.Log("Objet touché : " + hit.collider.name);
+        }
+    }
+}
