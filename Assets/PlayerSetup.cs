87ab092877beccad3ccb1f169f8bdf81b034eb53
Fafary <84011670+Fafary@users.noreply.github.com>
2021-05-19 15:45:43 +0200

    mirror partie 2

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using Mirror;
+public class PlayerSetup : MonoBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentToDisable;
+    
+    private void Start()
+    {
+        if( !isLocalPlayer)
+        {
+            //
+            for(int i = 0; i < componentToDisable.Length; i++)
+            {
+                componentToDisable[i].enabled = false;
+            }
+
+        }
+    }
+}
