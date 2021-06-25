7d27b9403b297bf9368735dc62921d6f7f416aab
ZEITTON <84009797+ZEITTON@users.noreply.github.com>
2021-06-25 17:31:30 +0200

    Death animation

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -4,8 +4,10 @@ public class PlayerAnimation : MonoBehaviour
 {
     Animator _animator;
     public PlayerMove playerMove;
+    public Player player;
     Vector3 move;
     bool isCrouching;
+    bool isDead;
 
     void Start()
     {
@@ -17,12 +19,14 @@ public class PlayerAnimation : MonoBehaviour
         //Variable
         move = playerMove.rb.velocity;
         isCrouching = playerMove.isCrouching;
+        isDead = player.isDead;
 
         //Animating
         float velocityZ = Vector3.Dot(move.normalized, transform.forward);
         float velocityX = Vector3.Dot(move.normalized, transform.right);
 
         _animator.SetBool("isCrouching", isCrouching);
+        _animator.SetBool("isDead", isDead);
         _animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
         _animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
     }
